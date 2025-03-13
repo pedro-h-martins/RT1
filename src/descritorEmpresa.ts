bc95ff92f6214ce5f8207a7dac97b3f57d4f6119
pedro-h-martins <pedrohenrimartinss@gmail.com>
2025-03-13 16:45:32 -0300

    Corrigido pequeno erro de formatacao

--- src/descritorEmpresa.ts
+++ src/descritorEmpresa.ts
@@ -9,11 +9,11 @@ export default class Descritor {
         console.log("Endereco") 
         console.log(`Rua: ${empresa.endereco.rua} Bairro: ${empresa.endereco.bairro} Cidade: ${empresa.endereco.cidade} numero: ${empresa.endereco.numero}`);
         
-        console.log("\nfuncionários:");
+        console.log("\n funcionários:");
         empresa.funcionarios.forEach(funcionario => {
             console.log(`Nome: ${funcionario.nome}`);
             console.log(`Matrícula: ${funcionario.matricula}`)
-            console.log(`CPF: ${funcionario.cpf}`)
+            console.log(`cpf: ${funcionario.cpf}`)
             console.log(`Rua: ${funcionario.endereco.rua} Bairro: ${funcionario.endereco.bairro} Cidade: ${funcionario.endereco.cidade} numero: ${funcionario.endereco.numero}`);
         })
     }
